92fd175a8df5808ba1ae3d012726b419791fcf81
gkresic <gordan.kresic@steatoda.com>
2015-10-15 20:13:25 +0200

    Extracted properties into separate singleton

--- src/main/java/hr/vsite/hive/Hive.java
+++ src/main/java/hr/vsite/hive/Hive.java
@@ -1,9 +1,5 @@
 package hr.vsite.hive;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,21 +17,13 @@ public class Hive {
 
     	long startTime = System.currentTimeMillis();
 
-    	Properties properties = new Properties();
-    	try (InputStream propertiesStream = ClassLoader.getSystemResourceAsStream("hive.properties")) {
-			properties.load(propertiesStream);
-			version = properties.getProperty("version");
-		} catch (IOException e) {
-			throw new RuntimeException("Could not load properties", e);
-		}
-    	
         log.info("**************************************************");
-        log.info("Welcome to Hive {}...", version);
+        log.info("Welcome to Hive {}...", HiveProperties.get().getVersion());
         log.info("**************************************************");
 
         HiveConfiguration.get();
         
-        log.info("Hive {} initialized in {} seconds", version, (System.currentTimeMillis() - startTime) / 1000);
+        log.info("Hive {} initialized in {} seconds", HiveProperties.get().getVersion(), (System.currentTimeMillis() - startTime) / 1000);
 
 	}
 	
@@ -43,11 +31,11 @@ public class Hive {
 
     	long startTime = System.currentTimeMillis();
 
-		log.info("Hive {} starting...", version);
+		log.info("Hive {} starting...", HiveProperties.get().getVersion());
 
 		// TODO
     	
-        log.info("Hive {} started in {} seconds", version, (System.currentTimeMillis() - startTime) / 1000);
+        log.info("Hive {} started in {} seconds", HiveProperties.get().getVersion(), (System.currentTimeMillis() - startTime) / 1000);
     	
 	}
 	
@@ -55,27 +43,25 @@ public class Hive {
 
 		long startTime = System.currentTimeMillis();
 		
-		log.info("Hive {} stopping...", version);
+		log.info("Hive {} stopping...", HiveProperties.get().getVersion());
 
 		// TODO
 		
-        log.info("Hive {} stopped in {} seconds", version, (System.currentTimeMillis() - startTime) / 1000);
+        log.info("Hive {} stopped in {} seconds", HiveProperties.get().getVersion(), (System.currentTimeMillis() - startTime) / 1000);
 		
 	}
 	
 	public void destroy() {
 
-		log.info("Hive {} is initializing shutdown...", version);
+		log.info("Hive {} is initializing shutdown...", HiveProperties.get().getVersion());
 
 		// TODO
 		
-		log.info("Hive {} successfully shut down - bye!", version);
+		log.info("Hive {} successfully shut down - bye!", HiveProperties.get().getVersion());
 		
 	}
 
 	private static final Logger log = LoggerFactory.getLogger(Hive.class);
 	private static Hive instance = null;
-
-	private String version = "0.TODO";	// TODO
 
 }
---
+++ src/main/java/hr/vsite/hive/HiveProperties.java
@@ -0,0 +1,36 @@
+package hr.vsite.hive;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class HiveProperties extends Properties {
+
+	public static HiveProperties get() {
+		if (instance == null)
+			instance = new HiveProperties();
+		return instance;
+	}
+	
+	public HiveProperties() {
+
+		super();
+
+    	try (InputStream propertiesStream = ClassLoader.getSystemResourceAsStream("hive.properties")) {
+			load(propertiesStream);
+			version = getProperty("version");
+		} catch (IOException e) {
+			throw new RuntimeException("Could not load properties", e);
+		}
+
+	}
+
+	public String getVersion() { return version; }
+
+	private static final long serialVersionUID = 1L;
+
+	private static HiveProperties instance = null;
+
+	private final String version;
+
+}
