8e0d3013f2ab2858ca6ea3b51621c4e64ace4aaa
gkresic <gordan.kresic@steatoda.com>
2015-10-26 20:39:02 +0100

    API versioning done

--- src/main/java/hr/vsite/hive/services/jetty/HiveServletModule.java
+++ src/main/java/hr/vsite/hive/services/jetty/HiveServletModule.java
@@ -4,6 +4,8 @@ import javax.inject.Singleton;
 
 import org.jboss.resteasy.plugins.server.servlet.HttpServlet30Dispatcher;
 
+import com.google.inject.Key;
+import com.google.inject.name.Names;
 import com.google.inject.servlet.ServletModule;
 
 import hr.vsite.hive.services.jetty.rest.v1.GuiceFilterDispatcherV1;
@@ -13,13 +15,13 @@ public class HiveServletModule extends ServletModule {
 	@Override
 	protected void configureServlets() {
 		
-		// TODO annotate instance with @Named for different API versions
-		bind(HttpServlet30Dispatcher.class).in(Singleton.class);
-		serve("/api/1/*").with(HttpServlet30Dispatcher.class);
+		// one servlet for all API verisons, so use Named annotation to make a difference
+		bind(HttpServlet30Dispatcher.class).annotatedWith(Names.named("V1")).to(HttpServlet30Dispatcher.class).in(Singleton.class);
+		serve("/api/1/*").with(Key.get(HttpServlet30Dispatcher.class, Names.named("V1")));
 
 		bind(GuiceFilterDispatcherV1.class).in(Singleton.class);
 		filter("/api/1/*").through(GuiceFilterDispatcherV1.class);
-		
+
 	}
 
 }
